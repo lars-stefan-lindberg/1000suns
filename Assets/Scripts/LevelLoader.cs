424eafd79ac30e2b2e4abc003744247856a131b9
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2024-05-16 11:56:49 +0200

    Add level loader.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+        }
+    }
+}
