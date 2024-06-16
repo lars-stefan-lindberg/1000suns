2df1f0730ee44e88ab72d44bab99d93e095a1b93
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2024-06-16 20:34:06 +0200

    wip: Level loading.

---
+++ Assets/Scripts/SceneLoadTrigger.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Scripting;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadTrigger : MonoBehaviour
+{
+    [SerializeField] private SceneField[] _scenesToLoad;
+    [SerializeField] private SceneField[] _scenesToUnload;
+
+    private GameObject _player;
+    private void Awake() {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject == _player) {
+            LoadScenes();
+            UnloadScenes();
+        }
+    }
+
+    private void LoadScenes() {
+        for(int i = 0; i < _scenesToLoad.Length; i++) 
+        {
+            bool isSceneLoaded = false;
+            for(int j = 0; j < SceneManager.sceneCount; j++) {
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if(loadedScene.name == _scenesToLoad[i].SceneName) {
+                    isSceneLoaded = true;
+                    break;
+                }
+            }
+
+            if(!isSceneLoaded) {
+                SceneManager.LoadSceneAsync(_scenesToLoad[i], LoadSceneMode.Additive);
+            }
+        }
+    }
+
+    private void UnloadScenes() {
+        for(int i = 0; i < _scenesToUnload.Length; i++) {
+            for(int j = 0; j < SceneManager.sceneCount; j++) {
+                Scene loadedScene = SceneManager.GetSceneAt(j);
+                if(loadedScene.name == _scenesToUnload[i].SceneName) {
+                    SceneManager.UnloadSceneAsync(_scenesToUnload[i]);
+                }
+            }
+        }
+    }
+}
