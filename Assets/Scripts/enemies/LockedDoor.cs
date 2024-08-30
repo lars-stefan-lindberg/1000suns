2a5cdfcf4db655ef0a4c10d0eadcca715e7d316c
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2024-08-30 14:45:35 +0200

    Make locked door death more forgiving.

--- Assets/Scripts/enemies/LockedDoor.cs
+++ Assets/Scripts/enemies/LockedDoor.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class LockedDoor : MonoBehaviour
 {
     private Animator _animator;
+    private BoxCollider2D _collider;
 
     void Awake() {
         _animator = GetComponent<Animator>();
+        _collider = GetComponent<BoxCollider2D>();
     }
 
     void OnCollisionEnter2D(Collision2D other) {
@@ -20,6 +22,7 @@ public class LockedDoor : MonoBehaviour
     [ContextMenu("PlayDeathAnimation")]
     public void PlayDeathAnimation() {
         _animator.SetTrigger("death");
+        _collider.enabled = false;
     }
 
     public void Kill() {
