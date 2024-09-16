3cf04141e605b25f9538b09c900892e6205206e9
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2024-09-16 11:22:55 +0200

    Fix prisoner spawn trigger.

--- Assets/Scripts/triggers/PrisonerSpawnTrigger.cs
+++ Assets/Scripts/triggers/PrisonerSpawnTrigger.cs
@@ -6,15 +6,19 @@ using UnityEngine;
 public class PrisonerSpawnTrigger : MonoBehaviour
 {
     public List<GameObject> prisonersToSpawn;
+    private bool _hasBeenSpawned = false;
 
     private void OnTriggerEnter2D(Collider2D collision) {
+        if(_hasBeenSpawned) {
+            return;
+        }
         if (collision.gameObject.CompareTag("Player"))
         { 
             foreach (GameObject prisoner in prisonersToSpawn)
             {
                 prisoner.SetActive(true);
             }
+            _hasBeenSpawned = true;
         }
-        this.gameObject.SetActive(false);
     }   
 }
