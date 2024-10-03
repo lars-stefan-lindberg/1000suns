e16f42b906d5421facc8ef1272e49475bc878504
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2024-10-03 11:14:15 +0200

    Add parallax to vertical room.

---
+++ Assets/Scripts/managers/BackgroundManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    private Camera _mainCamera;
+    [SerializeField] private float _parallaxEffect;
+    private float _startPosition;
+    void Start()
+    {
+        _startPosition = transform.position.y;
+        _mainCamera = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        float distance = _mainCamera.transform.position.y * _parallaxEffect;
+        transform.position = new Vector2(transform.position.x, _startPosition + distance);
+    }
+}
