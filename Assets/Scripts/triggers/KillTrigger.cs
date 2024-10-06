d13d0218e22b990ad2bf8abb647134f091b28605
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2024-10-06 14:14:13 +0200

    KillTrigger: possibility to only kill player (falling prisoners room).

--- Assets/Scripts/triggers/KillTrigger.cs
+++ Assets/Scripts/triggers/KillTrigger.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class KillTrigger : MonoBehaviour
 {
+    [SerializeField] private bool _onlyKillPlayer = false;
+
     void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player")) {
             Reaper.obj.KillPlayerGeneric();
         }
-        if(other.gameObject.CompareTag("Enemy")) {
+        if(other.gameObject.CompareTag("Enemy") && !_onlyKillPlayer) {
             Reaper.obj.KillPrisoner(other.gameObject.GetComponent<Prisoner>());
         }
     }
