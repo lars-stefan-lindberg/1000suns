7caacb05d8c97e66bd8ef10e20af6a76234d2a77
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2024-12-03 19:29:20 +0100

    Music to ambience before power up rooms.

---
+++ Assets/Scripts/audio/SwitchToAmbienceOnlyTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+public class SwitchToAmbienceOnlyTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            StartCoroutine(FadeOutMusicAndStartAmbience());
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    private IEnumerator FadeOutMusicAndStartAmbience() {
+        float musicVolume = SoundMixerManager.obj.GetMusicVolume();
+        StartCoroutine(SoundMixerManager.obj.StartMusicFade(5f, 0.001f));
+        AmbienceManager.obj.PlayAmbience();
+        while(SoundMixerManager.obj.GetMusicVolume() > 0.001f) {
+            yield return null;
+        }
+        MusicManager.obj.StopPlaying();
+        SoundMixerManager.obj.SetMusicVolume(musicVolume);
+    }
+}
