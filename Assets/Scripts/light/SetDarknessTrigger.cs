35408b424f02ef1d3c2c5899c435e7b76fefdd13
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2025-01-02 17:03:59 +0100

    Change darkness color dynamically.

---
+++ Assets/Scripts/light/SetDarknessTrigger.cs
@@ -0,0 +1,35 @@
+using FunkyCode;
+using UnityEngine;
+
+//Default darkness lighting rgb: 545454 (in case it gets overwritten)
+public class SetDarknessTrigger : MonoBehaviour
+{
+    [SerializeField] private Color _darknessColor;
+    [Range(0.1f, 10f), SerializeField] private float _fadeSpeed = 5f;
+
+    private bool IsFading = false;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            if(LightingManager2D.Get().profile.DarknessColor != _darknessColor) {
+                IsFading = true;
+            }
+        }
+    }
+
+    void Update() {
+        if(IsFading) {
+            Color darknessColor = LightingManager2D.Get().profile.DarknessColor;
+            if(darknessColor != _darknessColor) {
+                LightingManager2D.Get().profile.DarknessColor = Color.Lerp(darknessColor, _darknessColor, _fadeSpeed * Time.deltaTime); 
+            } else {
+                IsFading = false;
+                LightingManager2D.Get().profile.DarknessColor = _darknessColor;
+            }
+        }
+    }
+
+    public void StartFade() {
+        IsFading = true;
+    }
+}
