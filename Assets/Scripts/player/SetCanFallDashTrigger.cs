aa91fdcf2565fa5566f09886231799ff6e48d4b7
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2025-01-02 17:24:24 +0100

    Disable fall dash tutorial for now. Use dialogue later instead.

--- Assets/Scripts/player/SetCanFallDashTrigger.cs
+++ Assets/Scripts/player/SetCanFallDashTrigger.cs
@@ -8,7 +8,7 @@ public class SetCanFallDashTrigger : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
             Player.obj.CanFallDash = true;
-            TutorialFooterManager.obj.StartFadeIn();
+            //TutorialFooterManager.obj.StartFadeIn();
             GetComponent<BoxCollider2D>().enabled = false;
         }
     }
