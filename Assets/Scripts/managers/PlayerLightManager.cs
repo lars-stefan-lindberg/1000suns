2ac8fda7093db73128edb254a708abbb4d5abcb6
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2025-04-16 10:08:45 +0200

    Iteration on cave room 26 and 27.

--- Assets/Scripts/managers/PlayerLightManager.cs
+++ Assets/Scripts/managers/PlayerLightManager.cs
@@ -3,43 +3,38 @@ using UnityEngine;
 public class PlayerLightManager : MonoBehaviour
 {
     [SerializeField] private GameObject _light;
-    [SerializeField] private Transform _playerTransform;
-    [SerializeField] private Transform _blobTransform;
     [SerializeField] private float _transitionSpeed = 5f;
-    
+
     private bool _isTransitioning = false;
-    private Vector3 _targetPosition;
+    private Transform _lastActiveTransform = null;
 
     void Update()
     {
-        //TODO make this work
-        // Transform activeTransform = PlayerManager.obj.GetPlayerTransform();
-        
-        // // If we just switched characters, start a transition
-        // if (activeTransform != null && (_playerTransform != null && activeTransform == _playerTransform && _isTransitioning == false && _light.transform.position != _playerTransform.position) ||
-        //     (_blobTransform != null && activeTransform == _blobTransform && _isTransitioning == false && _light.transform.position != _blobTransform.position))
-        // {
-        //     _isTransitioning = true;
-        //     _targetPosition = activeTransform.position;
-        // }
-        
-        // // Handle the transition
-        // if (_isTransitioning)
-        // {
-        //     _light.transform.position = Vector3.Lerp(_light.transform.position, _targetPosition, Time.deltaTime * _transitionSpeed);
-            
-        //     // Check if we're close enough to end the transition
-        //     if (Vector3.Distance(_light.transform.position, _targetPosition) < 0.05f)
-        //     {
-        //         _isTransitioning = false;
-        //     }
-        // }
-        // else if (activeTransform != null)
-        // {
-        //     // When not transitioning, just update the target position to follow the active player
-        //     _targetPosition = activeTransform.position;
-        //     _light.transform.position = _targetPosition;
-        // }
-        _light.transform.position = PlayerManager.obj.GetPlayerTransform().position;
+        Transform activeTransform = PlayerManager.obj.GetPlayerTransform();
+        if (activeTransform == null) return;
+
+        // Detect transformation event
+        if (_lastActiveTransform != null && activeTransform != _lastActiveTransform)
+        {
+            _isTransitioning = true;
+        }
+
+        if (_isTransitioning)
+        {
+            Vector3 targetPos = activeTransform.position;
+            _light.transform.position = Vector3.Lerp(_light.transform.position, targetPos, Time.deltaTime * _transitionSpeed);
+            // End transition if close enough to the current position
+            if (Vector3.Distance(_light.transform.position, targetPos) < 0.05f)
+            {
+                _light.transform.position = targetPos;
+                _isTransitioning = false;
+            }
+        }
+        else
+        {
+            _light.transform.position = activeTransform.position;
+        }
+
+        _lastActiveTransform = activeTransform;
     }
 }
