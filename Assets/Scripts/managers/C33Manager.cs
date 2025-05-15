dd397a729fc82aeb30ec0d717722cada39c76794
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2025-05-15 09:45:49 +0200

    Iteration on cave room 33.

--- Assets/Scripts/managers/C33Manager.cs
+++ Assets/Scripts/managers/C33Manager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class C33Manager : MonoBehaviour
 {
     [SerializeField] private Transform _blockSpawnStartPosition;
     [SerializeField] private Transform _blockSpawnEndPosition;
     [SerializeField] private GameObject _blockPrefab;
+    [SerializeField] private GameObject _blockingWall;
+    [SerializeField] private float _blockingWallFadeMultiplier = 1.5f;
+    [SerializeField] private Transform _caveAvatarFleeTargetPosition;
     public float _attackInterval = 2f;
     public int _numberOfAttacks = 20;
     private int _attackCount = 0;
@@ -13,20 +17,35 @@ public class C33Manager : MonoBehaviour
     private float _timer = 1.5f;
     private BoxCollider2D _collider;
     private LayerMask _blockLayerMask;
+    private Tilemap _blockingWallTilemap;
+
 
     void Start() {
+        _collider = GetComponent<BoxCollider2D>();
         if(LevelManager.obj.IsLevelCompleted("C33")) {
-            Destroy(gameObject);
+            _collider.enabled = false;
+            _blockingWall.SetActive(true);
+            _blockingWall.GetComponentInChildren<Tilemap>().color = new Color(1, 1, 1, 1);
             return;
         }
-        _collider = GetComponent<BoxCollider2D>();
+        _blockingWallTilemap = _blockingWall.GetComponentInChildren<Tilemap>();
         _blockLayerMask = LayerMask.GetMask("Block");
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
         if(collision.CompareTag("Player")) {
-            _startAttackSequence = true;
             _collider.enabled = false;
+            _startAttackSequence = true;
+            _blockingWall.SetActive(true);
+            SoundFXManager.obj.PlayBrokenFloorReappear(transform);
+            StartCoroutine(FadeInBlockingWall());
+        }
+    }
+
+    private IEnumerator FadeInBlockingWall() {
+        while(_blockingWallTilemap.color.a < 1) {
+            _blockingWallTilemap.color = new Color(_blockingWallTilemap.color.r, _blockingWallTilemap.color.g, _blockingWallTilemap.color.b, Mathf.MoveTowards(_blockingWallTilemap.color.a, 1, _blockingWallFadeMultiplier * Time.deltaTime));
+            yield return null;
         }
     }
 
@@ -64,6 +83,7 @@ public class C33Manager : MonoBehaviour
             block.GetComponent<Block>().SetGravity(1.5f);
             _attackCount++;
             if(_attackCount >= _numberOfAttacks) {
+                CaveAvatar.obj.SetTarget(_caveAvatarFleeTargetPosition, 10);
                 _startAttackSequence = false;
             }
         }
