a8172f394c57a034cbc6455b5493b0611e561cfa
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2025-06-06 08:49:58 +0200

    Add stinger in mirror room.

---
+++ Assets/Scripts/triggers/MirrorRoomStingerTrigger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MirrorRoomStingerTrigger : MonoBehaviour
+{
+    private BoxCollider2D _collider;
+    
+    void Start() {
+        _collider = GetComponent<BoxCollider2D>();
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            if(GameEventManager.obj.MirrorConversationEnded) {
+                return;
+            }
+            SoundFXManager.obj.PlayCaveAvatarEvilEyesTransition();
+            _collider.enabled = false;
+        }
+    }
+}
