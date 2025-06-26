acefebf945620594d932dec7fb098f27d872e495
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2025-06-26 16:11:16 +0200

    fix: don't light up torches twice in final cave room.

--- Assets/Scripts/triggers/LightUpTorchTrigger.cs
+++ Assets/Scripts/triggers/LightUpTorchTrigger.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class LightUpTorchTrigger : MonoBehaviour
 {
     [SerializeField] private Torch _torch;
+    private BoxCollider2D _collider;
+
+    void Awake() {
+        _collider = GetComponent<BoxCollider2D>();
+    }
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player")) {
             _torch.LightUp();
+            _collider.enabled = false;
         }
     }
 }
