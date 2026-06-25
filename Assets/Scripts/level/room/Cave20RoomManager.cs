2821dad99e75619a11ce05a7606b5c3a08452956
Stefan Lindberg <lars.stefan.lindberg@gmail.com>
2026-06-25 10:58:40 +0200

    Add secret room + collectible.

---
+++ Assets/Scripts/level/room/Cave20RoomManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Cave20RoomManager : MonoBehaviour
+{
+    [SerializeField] private GameEventId _secretRevealed;
+    [SerializeField] private GameObject _secretWall;
+
+    void Start()
+    {
+        if(GameManager.obj.HasEvent(_secretRevealed)) {
+            _secretWall.SetActive(false);
+        }
+    }
+
+    public void SetSecretRevealed() {
+        GameManager.obj.RegisterEvent(_secretRevealed);
+    }
+}
