9e036b627eba68f9f0d970da0827c04f4b979e22
lars-stefan-lindberg <lars.stefan.lindberg@gmail.com>
2026-08-03 12:20:01 +0200

    Add guiding mushrooms to Cave-48.

--- Assets/Scripts/level/room/Cave48RoomManager.cs
+++ Assets/Scripts/level/room/Cave48RoomManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using FMODUnity;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,11 +9,22 @@ public class Cave48RoomManager : MonoBehaviour
     [SerializeField] private GameEventId _hiddenFloorRevealed;
     [SerializeField] private GameEventId _sootFliedOff;
     [SerializeField] private GameEventId _cave52ConversationCompleted;
+    [SerializeField] private GameEventId _guidingMushroomsGrown;
     [SerializeField] private GameObject _caveAvatarFlyOffTarget;
     [SerializeField] private GameObject _cutsceneTrigger;
+    [SerializeField] private GameObject[] _guidingMushrooms;
+    [SerializeField] private EventReference _mushroomGrowSfx;
 
     void Start()
     {
+        if(GameManager.obj.HasEvent(_guidingMushroomsGrown)) {
+            foreach (var mushroom in _guidingMushrooms)
+            {
+                mushroom.GetComponent<SpriteRenderer>().enabled = true;
+                mushroom.GetComponent<LightSprite2DFadeManager>().SetFadedInState();
+            }
+        }
+
         //If not following player -> assuming that we are Eli
         if(!CaveAvatar.obj.IsFollowingPlayer) {
             if(GameManager.obj.HasEvent(_sootFliedOff)) {
@@ -52,6 +64,31 @@ public class Cave48RoomManager : MonoBehaviour
 
         PlayerBlobMovement.obj.UnFreeze();
         GameManager.obj.RegisterEvent(_sootFliedOff);
+        SaveManager.obj.SaveGame(SceneManager.GetActiveScene().name);
+        yield return null;
+    }
+
+    public void GrowGuidingMushrooms() {
+        if(GameManager.obj.HasEvent(_guidingMushroomsGrown))
+            return;
+        
+        StartCoroutine(GrowMushrooms());
+    }
+
+    private IEnumerator GrowMushrooms() {
+        foreach (var mushroom in _guidingMushrooms)
+        {
+            mushroom.GetComponent<SpriteRenderer>().enabled = true;
+            mushroom.GetComponent<SpriteRenderer>().sprite = null;
+            mushroom.GetComponent<Animator>().enabled = true;
+            SoundFXManager.obj.PlayAtPosition(_mushroomGrowSfx, mushroom.transform.position);
+            
+            LightSprite2DFadeManager lightFadeManager = mushroom.GetComponent<LightSprite2DFadeManager>();
+            lightFadeManager.StartFadeIn();
+            
+            yield return new WaitForSeconds(1f);
+        }
+        GameManager.obj.RegisterEvent(_guidingMushroomsGrown);
         SaveManager.obj.SaveGame(SceneManager.GetActiveScene().name);
         yield return null;
     }
