3218ea11e95a27e6808b00dbee9413a6fda232e3
lars-stefan-lindberg <lars.stefan.lindberg@gmail.com>
2026-08-06 09:07:09 +0200

    Add Dee monologue after first prisoner fight.

--- Assets/Scripts/managers/FirstPrisonerManager.cs
+++ Assets/Scripts/managers/FirstPrisonerManager.cs
@@ -1,15 +1,19 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
-public class FirstPrisonerManager : MonoBehaviour
+public class FirstPrisonerManager : MonoBehaviour, ISkippable
 {
     [SerializeField] private GameEventId _firstPrisonerFightEndedEli;
     [SerializeField] private GameEventId _firstPrisonerFightEndedDee;
     [SerializeField] private GameObject _bossGameObjects;
     [SerializeField] private GameObject _blockingWall;
     [SerializeField] private Tilemap _blockingWallTilemap;
+    [SerializeField] private ConversationManager _conversationManager;
+
+    private Coroutine _cutsceneCoroutine;
 
     void Start() {
         CaveTimelineId.Id caveTimeline = GameManager.obj.GetCaveTimeline().GetCaveTimelineId();
@@ -35,9 +39,48 @@ public class FirstPrisonerManager : MonoBehaviour
         CaveTimelineId.Id caveTimeline = GameManager.obj.GetCaveTimeline().GetCaveTimelineId();
         if(caveTimeline == CaveTimelineId.Id.Eli) {
             GameManager.obj.RegisterEvent(_firstPrisonerFightEndedEli);
+            SaveManager.obj.SaveGame(SceneManager.GetActiveScene().name);
         } else if(caveTimeline == CaveTimelineId.Id.Dee) {
-            GameManager.obj.RegisterEvent(_firstPrisonerFightEndedDee);
+            PauseMenuManager.obj.RegisterSkippable(this);
+            ShadowTwinMovement.obj.Freeze();
+            _conversationManager.OnConversationEnd += OnConversationCompleted;
+            StartCoroutine(PlayDialogue());
         }
+    }
+    private IEnumerator PlayDialogue() {
+        yield return new WaitForSeconds(2.5f);
+        _conversationManager.StartConversation();
+        yield return null;
+    }
+
+    public void RequestSkip() {
+        if(_cutsceneCoroutine != null) {
+            StopCoroutine(_cutsceneCoroutine);
+        }
+
+        _conversationManager.OnConversationEnd -= OnConversationCompleted;
+        _conversationManager.HardStopConversation();
+        _conversationManager.CleanUp();
+
+        GameManager.obj.RegisterEvent(_firstPrisonerFightEndedDee);
+        SaveManager.obj.SaveGame(SceneManager.GetActiveScene().name);
+
+        StartCoroutine(ResumeGameplay());
+    }
+    private IEnumerator ResumeGameplay() {
+        SceneFadeManager.obj.StartFadeIn();
+        while(SceneFadeManager.obj.IsFadingIn) {
+            yield return null;
+        }
+        ShadowTwinMovement.obj.UnFreeze();
+        GameManager.obj.IsPauseAllowed = true;
+        yield return null;
+    }
+
+    private void OnConversationCompleted() {
+        PauseMenuManager.obj.UnregisterSkippable();
+        ShadowTwinMovement.obj.UnFreeze();
+        GameManager.obj.RegisterEvent(_firstPrisonerFightEndedDee);
         SaveManager.obj.SaveGame(SceneManager.GetActiveScene().name);
     }
 }
