bc045538018474f408ff40028bea366f7594f95b
lars-stefan-lindberg <lars.stefan.lindberg@gmail.com>
2026-09-08 16:52:02 +0200

    Add skip cutscene to Soot-floating platform tutorial.

--- Assets/Scripts/level/room/Cave25RoomManager.cs
+++ Assets/Scripts/level/room/Cave25RoomManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Cave25RoomManager : MonoBehaviour
+public class Cave25RoomManager : MonoBehaviour, ISkippable
 {
     [SerializeField] private ConversationManager _conversationManager;
     [SerializeField] private GameEventId _cutsceneCompleted;
@@ -11,6 +11,7 @@ public class Cave25RoomManager : MonoBehaviour
     [SerializeField] private Transform _sootFlyTarget2;
     [SerializeField] private FloatyPlatform _floatingPlatform;
     [SerializeField] private GameObject _cutsceneCamera;
+    private Coroutine _cutsceneCoroutine;
 
     public void StartCutscene() {
         if(GameManager.obj.HasEvent(_cutsceneCompleted)) {
@@ -18,10 +19,11 @@ public class Cave25RoomManager : MonoBehaviour
         }
         PlayerMovement.obj.Freeze();
 
-        StartCoroutine(StartCutsceneCoroutine());
+        _cutsceneCoroutine = StartCoroutine(StartCutsceneCoroutine());
     }
 
     private IEnumerator StartCutsceneCoroutine() {
+        PauseMenuManager.obj.RegisterSkippable(this);
         _cutsceneCamera.SetActive(true);
         yield return new WaitForSeconds(2f);
         _conversationManager.enabled = true;
@@ -39,23 +41,47 @@ public class Cave25RoomManager : MonoBehaviour
         _conversationManager.StartConversation();
     }
     
-    
     private void OnConversationCompleted() { 
         _conversationManager.OnConversationEnd -= OnConversationCompleted;
         _conversationManager.enabled = false;
 
-        StartCoroutine(OnConversationCompletedCoroutine());
-    }
-    
-    private IEnumerator OnConversationCompletedCoroutine() { 
         _cutsceneCamera.SetActive(false);
+        PauseMenuManager.obj.UnregisterSkippable();
         PlayerMovement.obj.UnFreeze();
         CaveAvatar.obj.IsFollowingPlayer = true;
         GameManager.obj.RegisterEvent(_cutsceneCompleted);
         SaveManager.obj.SaveGame(SceneManager.GetActiveScene().name);
-        yield return null;
     }
 
+    public void RequestSkip() {
+        if(_cutsceneCoroutine != null) {
+            StopCoroutine(_cutsceneCoroutine);
+        }
+
+        _conversationManager.HardStopConversation();
+        _conversationManager.CleanUp();
+        _conversationManager.OnConversationEnd -= OnConversationCompleted;
+        _conversationManager.enabled = false;
+
+        CaveAvatar.obj.FollowPlayer();
+        _floatingPlatform.Reset();
+        _cutsceneCamera.SetActive(false);
+
+        GameManager.obj.RegisterEvent(_cutsceneCompleted);
+        SaveManager.obj.SaveGame(SceneManager.GetActiveScene().name);
+
+        StartCoroutine(ResumeGameplay());
+    }
+
+    private IEnumerator ResumeGameplay() {
+        SceneFadeManager.obj.StartFadeIn();
+        while(SceneFadeManager.obj.IsFadingIn) {
+            yield return null;
+        }
+        PlayerMovement.obj.UnFreeze();
+        GameManager.obj.IsPauseAllowed = true;
+        yield return null;
+    }
 
     void OnDestroy() {
         _conversationManager.OnConversationEnd -= OnConversationCompleted;
