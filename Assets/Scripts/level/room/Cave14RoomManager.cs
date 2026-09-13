42046a96f3cc8ea37ee781be41deed4c7d59c64a
lars-stefan-lindberg <lars.stefan.lindberg@gmail.com>
2026-09-13 12:05:05 +0200

    Save falling block in Cave-14.

--- Assets/Scripts/level/room/Cave14RoomManager.cs
+++ Assets/Scripts/level/room/Cave14RoomManager.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Cave14RoomManager : MonoBehaviour
 {
     [SerializeField] private GameEventId _secretRevealed;
+    [SerializeField] private GameEventId _blockDropped;
     [SerializeField] private GameObject _secretWall;
+    [SerializeField] private GameObject _fallingBlock;
+    [SerializeField] private Transform _fallingBlockLowerThreshold;
 
     void Start()
     {
         if(GameManager.obj.HasEvent(_secretRevealed)) {
             _secretWall.SetActive(false);
         }
+        if(GameManager.obj.HasEvent(_blockDropped))
+            _fallingBlock.SetActive(false);
     }
 
     public void SetSecretRevealed() {
         GameManager.obj.RegisterEvent(_secretRevealed);
+    }
+
+    void FixedUpdate() {
+        if(GameManager.obj.HasEvent(_blockDropped)) {
+            return;
+        }
+        if(_fallingBlock.transform.position.y <= _fallingBlockLowerThreshold.position.y) {
+            GameManager.obj.RegisterEvent(_blockDropped);
+            SaveManager.obj.SaveGame(SceneManager.GetActiveScene().name);
+        }
     }
 }
